d0908cdc17bb43c8c897812a4d8008939d51f256
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2023-01-10 15:05:18 -0300

    façade

---
+++ src/structural/facade/builder-facade.ts
@@ -0,0 +1,16 @@
+import { MainDishBuilder } from '../../creational/builder/classes/main-dish-builder';
+
+export class BuilderFacade {
+  // Essa é a fachada para a pasta src/creational/builder/index.ts
+  private mainDishBuilder = new MainDishBuilder();
+  makeMeal1(): void {
+    this.mainDishBuilder.makeMeal();
+    console.log(this.mainDishBuilder.getMeal());
+    console.log(this.mainDishBuilder.getPrice());
+  }
+  makeMeal2(): void {
+    this.mainDishBuilder.reset();
+    const meal2 = this.mainDishBuilder.makeBeverage().getMeal();
+    console.log(meal2);
+  }
+}
